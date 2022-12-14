c672aabce7977f18a826b5204e1e617ad7f76605
Michael.Xing <michael.xing2@telus.com>
2022-12-14 15:50:27 -0700

    express example

---
+++ express_backend/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+const PORT = 3003;
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`user connected: ${socket.id}`);
+
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("broadcast", data);
+    // io.emit("broadcast", data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("sever is running on port " + PORT);
+});
--- react_sio/src/App.tsx
+++ react_sio/src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import Reac, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
 
-function App() {
-  const [count, setCount] = useState(0)
+const socket = io(`http://localhost:3003`);
+
+const App = () => {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState<string[]>([]);
+
+  const sendMessage = () => {
+    socket.emit("message", { message: message });
+  };
+
+  useEffect(() => {
+    socket.on("broadcast", (data) => {
+      setMessageReceived([data.message]);
+    });
+  }, [socket]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        placeholder="Message..."
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setMessage(event.target.value);
+        }}
+        value={message}
+      />
+      <button onClick={sendMessage}>Send Message</button>
+      <hr />
+      <h2>Messges:</h2>
+      <p>{messageReceived}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
